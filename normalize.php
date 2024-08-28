f228c89ba416fcd74202c00b25a01936d2176e23
eileen <emcnaughton@wikimedia.org>
2024-08-29 10:36:55 +1200

    Run civix upgrade, add Smarty3+ support

--- normalize.php
+++ normalize.php
@@ -27,26 +27,10 @@ function normalize_civicrm_config(&$config) {
 }
 
 /**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- */
-function normalize_civicrm_xmlMenu(&$files) {
-  _normalize_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implementation of hook_civicrm_install
  */
 function normalize_civicrm_install() {
   return _normalize_civix_civicrm_install();
-}
-
-/**
- * Implementation of hook_civicrm_uninstall
- */
-function normalize_civicrm_uninstall() {
-  return _normalize_civix_civicrm_uninstall();
 }
 
 /**
@@ -57,36 +41,6 @@ function normalize_civicrm_enable() {
   $session = CRM_Core_Session::singleton();
   $session->replaceUserContext(CRM_Utils_System::url('civicrm/admin/setting/reformat'));
   return _normalize_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- */
-function normalize_civicrm_disable() {
-  return _normalize_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function normalize_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _normalize_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implementation of hook_civicrm_managed
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- */
-function normalize_civicrm_managed(&$entities) {
-  return _normalize_civix_civicrm_managed($entities);
 }
 
 /**
